2ad6dc0348ea1099ebd47d7f71b6413af81ca22b
Anish <anish.kottu@duke.edu>
2020-03-18 12:56:33 -0400

    config updated

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://user:Hooperdb2020@vcm-13382.vm.duke.edu:5432/hooper'
 SQLALCHEMY_ECHO = True
 DEBUG = True
